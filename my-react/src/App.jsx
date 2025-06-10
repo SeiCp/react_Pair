ad9c6ca7872947ee2c5cc32114c41c98e39356fb
SeiCp <seycuartero@gmail.com>
2025-06-10 21:29:06 +0200

    changes

---
+++ my-react/src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './styles/App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+    <h1>Hola</h1>
+
+    <h2>Subtitle</h2>
+
+
+    </>
+  )
+}
+
+export default App
